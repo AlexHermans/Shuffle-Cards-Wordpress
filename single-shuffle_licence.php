068ccc39582b094f5d6120ed4b16034555db6a2a
AlexHermans <alexhermans90@gmail.com>
2018-03-15 11:05:16 +0100

    CSS adjustments for single-licence.php
    Added more css for the single-licence page. Right now the framework, save for everything concerning products, looks alright.

--- single-shuffle_licence.php
+++ single-shuffle_licence.php
@@ -16,14 +16,15 @@
 <?php get_header(); ?>
 <body>
 <?php global $post?>
+<?php $term = get_the_terms($post->ID, 'target_audience')[0]->slug;?>
 <header class="licence-single__header" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/img/licences/<?php echo $post->post_title?>/header_background.jpg);">
     <h1 class="licence-single__logo licence-logo" style="background-image: url(<?php echo get_template_directory_uri();?>/assets/img/licences/<?php echo $post->post_title?>/header_logo.png);"><?php echo $post->post_title; ?></h1>
 </header>
-<section class="section-licence-title <?php echo get_the_terms($post->ID, 'target_audience')[0]->slug; ?>">
+<section class="section-licence-title <?php echo $term; ?>">
     <figure class="figure licence__figure-rounded-corner">
         <div class="figure-rounded-corner__div figure-rounded-corner__div-left transparent"></div>
         <div class="figure-rounded-corner__div figure-rounded-corner__div-middle"></div>
-        <div class="figure-rounded-corner__div figure-rounded-corner__div-right"></div>
+        <div class="figure-rounded-corner__div figure-rounded-corner__div-right <?php echo $term; ?>"></div>
     </figure>
     <h2 class="licence-single__h2 h2-secondary-title"><?php echo get_the_title();?></h2>
 </section>
