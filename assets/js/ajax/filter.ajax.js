5758cbde09ab52f0b12b957347f2ae02c84050fb
AlexHermans <alexhermans90@gmail.com>
2018-05-21 16:02:51 +0200

    Colour button filter now changes on click
    See title

--- assets/js/ajax/filter.ajax.js
+++ assets/js/ajax/filter.ajax.js
@@ -6,22 +6,36 @@
     $(document).ready(function () {
         let $button = $('.categories__li');
         let $content = $('.licence');
+        let colours = [
+            'rgb(245, 155, 0)',
+            'rgb(146, 58, 128)',
+            'rgb(0, 138, 210)',
+            'rgb(226, 38, 27)',
+            'rgb(100, 179, 43)',
+        ];
+        let counter = 0
 
         $button.click(function (e) {
             e.preventDefault();
+            $button.css('background-color', '#7f8184')
+            $active_button = $(this);
 
             $.ajax({
                 type: 'POST',
                 url: myAjax.ajaxurl,
                 data: {
                     action: 'shuffle_load_results_per_category',
-                    cat: $(this).attr('id')
+                    cat: $active_button.attr('id')
                 },
                 beforeSend: function(){
                     $content.addClass('loading');
                 },
                 success: function(response){
                     $content.removeClass('loading');
+                    if(counter < colours.length-1){
+                        counter += 1;
+                    } else { counter = 0 }
+                    $active_button.css('background-color' , colours[counter])
                     $content.html(response);
                     layout();
                 },
