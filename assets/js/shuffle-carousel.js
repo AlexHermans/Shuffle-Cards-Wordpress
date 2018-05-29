d964e40894e51eacf15bc9a98aa4a9dd73d40c44
AlexHermans <alexhermans90@gmail.com>
2018-05-29 15:45:57 +0200

    Fixed the carousel
    The graphical glitch where the horizontal scrollbar appeared briefly between animations should no longer appear.

--- assets/js/shuffle-carousel.js
+++ assets/js/shuffle-carousel.js
@@ -4,36 +4,33 @@
 
 (function($) {
     $(document).ready(function(){
-        var items = $('.section-carousel__div');
-        var container = $('.carousel-front-page');
-        var current = $(items[0]);
-        var next = $(items[1]);
-        var counter = 0;
+        var $items = $('.section-carousel__div');
+        var $container = $('.carousel-front-page');
+        var $current = $($items[0]);
+        var $next = $($items[1]);
+        var $counter = 0;
 
-        items.not(current).addClass('display-none');
+        $items.not($current).css('opacity', 0);
 
         setInterval(function () {
-            if (counter < 400){
-                counter += 100;
-
-                next.removeClass('display-none')
-
-                container.animate({
-                    left: '-' + counter + '%',
-                });
-
-                current.removeClass('current');
-                current = next.addClass('current');
-                next = next.next();
+            if ($counter < 4){
+                $counter += 1;
+                $current.animate({
+                    left: '-2%'
+                })
+                $next.css('opacity', 1);
+                $current = $current.next();
+                $next = $next.next();
+                console.log($current)
 
             } else {
-                current.removeClass('current')
-                counter = 0;
-                container.css('left', '0');
-                current = $(items[0]);
-                current.addClass('current')
-                next = current.next();
-                items.not(current).addClass('display-none');
+                $counter = 0;
+                $current = $($items[0]);
+                $current.css('opacity', 1);
+                console.log($current)
+                $next = $current.next();
+                $items.not($current).css('opacity', 0);
+                $items.css('left', 0);
             }
         }, 3000)
     });
